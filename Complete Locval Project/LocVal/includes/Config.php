9997120b4dedd0768fd69f216c893e0dd3ef04fe
karthi-jsdev <karthi.jsdev@gmail.com>
2022-09-29 21:10:25 +0530

    cofig added

--- Complete Locval Project/LocVal/includes/Config.php
+++ Complete Locval Project/LocVal/includes/Config.php
@@ -1,8 +1,18 @@
 <?php
 	//$dbh = mysql_connect("localhost","locvaluc","0909@Locvalu") or die ('I cannot connect to the database because: ' . mysql_error());
 	//mysql_select_db("locvaluc_locationvalue",$dbh) or die ('I cannot select to the database because: '.mysql_error());
-	$dbh = mysql_connect("localhost","root","") or die ('I cannot connect to the database because: ' . mysql_error());
-	mysql_select_db("locationvalue",$dbh) or die ('I cannot select to the database because: '.mysql_error());
+	// $dbh = mysql_connect("localhost","root","") or die ('I cannot connect to the database because: ' . mysql_error());
+	// mysql_select_db("locationvalue",$dbh) or die ('I cannot select to the database because: '.mysql_error());
 
-	$con=mysqli_connect("localhost","locvaluc","0909@Locvalu","locvaluc_locationvalue");
+	// $con=mysqli_connect("localhost","locvaluc","0909@Locvalu","locvaluc_locationvalue");
+
+	$mysql_host = "localhost";
+	$mysql_user = "root";
+	$mysql_password = "";
+	$mysql_database = "locationvalue";
+	$dbh = mysqli_connect ($mysql_host, $mysql_user, $mysql_password) or die ('I cannot connect to the database because: ' . mysqli_error());
+	mysqli_select_db ($dbh,$mysql_database) or die ('I cannot select the database because: ' . mysqli_error());
+	$_SESSION['connection'] = mysqli_connect($mysql_host, $mysql_user, $mysql_password,$mysql_database);
+
+
 ?>
